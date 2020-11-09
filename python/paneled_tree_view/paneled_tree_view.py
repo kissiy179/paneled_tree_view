7d17872bf1aad7432a9775cc034cbdac58c86d7a
kissiy179 <kissiy179@gmail.com>
2020-11-09 20:36:17 +0900

    paneled_tree_view.py内のサンプルファイルをsample_view.pyに分離

--- python/paneled_tree_view/paneled_tree_view.py
+++ python/paneled_tree_view/paneled_tree_view.py
@@ -566,38 +566,3 @@ class PaneledTreeView(QtWidgets.QTreeView):
 
             header.setSectionResizeMode(column, QtWidgets.QHeaderView.ResizeToContents)
 
-def show():
-    win = QtWidgets.QDialog(maya_win)
-    vlo = QtWidgets.QVBoxLayout()
-    win.setLayout(vlo)
-
-    view = PaneledTreeView()
-    model = PanelItemModel()
-
-    model = QtWidgets.QFileSystemModel()
-    root_dir = os.path.realpath(os.path.join(__file__, '..', '..', '..'))
-    index = model.setRootPath(root_dir)
-
-    # model = QtCore.QStringListModel([str(i) for i in range(100)])
-    view.setModel(model)
-    view.setRootIndex(index)
-
-    # for i in range(100):
-    #     item = PanelItem(AbstractPanelItemData(value=__file__))
-    #     model.root_item.add_child(item)
-
-    #     for j in range(2):
-    #         item_ = PanelItem(AbstractPanelItemData(value=__file__))
-    #         item.add_child(item_)
-
-    #         for k in range(5):
-    #             item__ = PanelItem(AbstractPanelItemData(value=__file__))
-    #             item_.add_child(item__)
-
-    #             for l in range(3):
-    #                 item___ = PanelItem(AbstractPanelItemData(value=__file__))
-    #                 item__.add_child(item___)
-
-    vlo.addWidget(view)
-    win.resize(600, 400)
-    win.show()
