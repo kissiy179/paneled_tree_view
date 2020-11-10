c60adfda51579163df10092770c4b2c75ec56c19
kissiy179 <kissiy179@gmail.com>
2020-11-10 21:02:03 +0900

    setup.pyを共通のものに変更

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-
 from glob import glob
 from os.path import basename
 from os.path import splitext
+from os.path import exists
 from setuptools import setup
 from setuptools import find_packages
 
 def _requires_from_file(filename):
+    if not exists(filename):
+        return []
+    
     return open(filename).read().splitlines()
 
 setup(
-    name="paneled_tree_view",
-    version="0.1.0",
-    license="MIT",
-    description=u"パネルぽいツリービュー",
-    author="satoshi kishimoto",
-    url="",
-    packages=find_packages("python"),
+    name='paneled_tree_view',
+    version='0.1.0',
     package_dir={"": "python"},
+    packages=find_packages("python"),
     py_modules=[splitext(basename(path))[0] for path in glob('python/*.py')],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7",
     install_requires=_requires_from_file('requirements.txt'),
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest", "pytest-cov"]  
 )
